8ffde6404386bdcc65ac3c5e02a8c4a779bcf78e
Aman Prasad <310amanprasad@gmail.com>
2024-08-02 12:19:59 +0530

    added all file to git

---
+++ script.js
@@ -0,0 +1,39 @@
+var todo_list = [{
+    name:"LEARN JAVA",
+    dueDate:"3-10-2003"
+}];
+
+function add_on_div(){
+    var add_div = '';
+    for(let i = 0 ; i<todo_list.length;i++){
+        const todolist = todo_list[i];
+        const {name,dueDate} = todolist;
+        const temp = `
+                <div>${name}</div>
+                <div>${dueDate}</div>
+                <button 
+                onclick = "
+                    todo_list.splice(${i}, 1);
+                    add_on_div();
+                " class="delete_button">
+                    delete
+                </button>
+            `;
+        add_div += temp;
+    }
+    document.querySelector(".view").innerHTML = add_div;
+}
+
+
+function add_to_list(){
+    const str = document.querySelector(".user_input");
+    const strDate = document.querySelector(".user_date");
+    const name = str.value;
+    const dueDate = strDate.value;
+    if(str.value !== ''){
+    todo_list.push({name,dueDate});
+    }
+    str.value='';
+    add_on_div();
+}
+add_on_div();
